7c16e074217b6eec0e2386597666d70a7ed7b8bc
nazm0l <tanjirahmednazmul@gmail.com>
2022-07-29 03:20:14 +0600

    dotenv used for secure pass.

--- index.js
+++ index.js
@@ -3,14 +3,14 @@ const cors = require("cors");
 const { MongoClient, ServerApiVersion, ObjectId } = require("mongodb");
 const app = express();
 const port = process.env.PORT || 3000;
+require("dotenv").config();
 
 app.use(cors());
 app.use(express.json());
 
 // MongoDB Connection
 
-const uri =
-  "mongodb+srv://crud:crud12@cluster0.m0aff.mongodb.net/?retryWrites=true&w=majority";
+const uri = `mongodb+srv://${process.env.USER_NAME}:${process.env.PASSWORD}@cluster0.m0aff.mongodb.net/?retryWrites=true&w=majority`;
 const client = new MongoClient(uri, {
   useNewUrlParser: true,
   useUnifiedTopology: true,
